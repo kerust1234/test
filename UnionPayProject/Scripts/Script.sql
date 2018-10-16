d7fc47166c344557d485d2f94ed76378788cf570
CN085258 <CN085258@CNWSGZISDL403.na.intranet.msd>
2018-10-16 12:14:18 +0800

    push to upstream

--- UnionPayProject/Scripts/Script.sql
+++ UnionPayProject/Scripts/Script.sql
@@ -1,3 +1,3 @@
 查询Amanet用户账号密码
 Select * from db2inst1.zdb001w where d01ada=7584860 with ur;
-Select * from db2inst1.zdb001w where D01ENC='Y' with ur;
+Select * from db2inst1.zdb001w where D01ENC='Y' fetch first 100 rows ONLY WITH ur;
